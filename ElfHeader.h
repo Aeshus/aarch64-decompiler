ab519b986206a5893f5c23af981b41dc93a1d423
Thomas (Aeshus) <git@aeshus.com>
2026-02-01 15:52:21 -0600

    Finish ELF header parsing

--- ElfHeader.h
+++ ElfHeader.h
@@ -93,13 +93,78 @@ enum class ElfOSABI {
     ABIStandalone = ELFOSABI_STANDALONE,
 };
 
+/**
+ * ELF machine/ISA.
+ */
+enum class ElfMachine {
+    /**
+     * No machine.
+     */
+    MachineNone = EM_NONE,
+
+    /**
+     * AMD64 / x86_64.
+     */
+    MachineAMD64 = EM_X86_64,
+
+    /**
+     * ARM (32-bit).
+     */
+    MachineARM = EM_ARM,
+
+    /**
+     * ARM AARCH64.
+     */
+    MachineAARCH64 = EM_AARCH64,
+};
+
 class ElfHeader {
 public:
     ElfClass elf_class;
     ElfData elf_data;
     ElfVersion elf_version;
     ElfOSABI elf_os_abi;
-    uint8_t elf_abi_version;
+    unsigned char elf_abi_version;
+
+    ElfMachine elf_machine;
+    uint64_t elf_entry;
+    uint64_t elf_program_header_offset;
+    uint64_t elf_section_header_offset;
+
+    /**
+     * CPU/Machine flags, not handled yet.
+     */
+    uint32_t elf_flags;
+
+    /**
+     * ELF header size (in bytes)
+     */
+    uint16_t elf_header_size;
+
+    /**
+     * The size of each entry in the program header table.
+     */
+    uint16_t elf_program_header_entry_size;
+
+    /**
+     * The count of entries in the program header table.
+     */
+    uint16_t elf_program_header_entry_count;
+
+    /**
+     * The size of the entry in the section header table.
+     */
+    uint16_t elf_section_header_entry_size;
+
+    /**
+     * The count of entries in the section header table.
+     */
+    uint16_t elf_section_header_entry_count;
+
+    /**
+     * The index in the section header table that points to the string section.
+     */
+    uint16_t elf_section_string_index;
 
     explicit ElfHeader(std::span<const std::byte> data) {
         // The struct sizes are different between the 32-bit and 64-bit variants,
@@ -140,9 +205,75 @@ public:
 private:
     std::variant<Elf64_Ehdr, Elf32_Ehdr> header{};
 
-    uint8_t getIdentData(int index) {
+    unsigned char getIdentData(int index) {
         return std::visit([index](const auto &hdr) {
             return hdr.e_ident[index];
+        }, header);
+    }
+
+    uint16_t getMachine() {
+        return std::visit([](const auto &hdr) {
+            return hdr.e_machine;
+        }, header);
+    }
+
+    uint64_t getEntry() {
+        if (is64bit()) {
+            return std::get<Elf64_Ehdr>(header).e_entry;
+        }
+
+        return std::get<Elf32_Ehdr>(header).e_entry;
+    }
+
+    uint64_t getProgramOffset() {
+        if (is64bit()) {
+            return std::get<Elf64_Ehdr>(header).e_phoff;
+        }
+
+        return std::get<Elf32_Ehdr>(header).e_phoff;
+    }
+
+    uint64_t getSectionOffset() {
+        if (is64bit()) {
+            return std::get<Elf64_Ehdr>(header).e_shoff;
+        }
+
+        return std::get<Elf32_Ehdr>(header).e_shoff;
+    }
+
+    uint32_t getFlags() {
+        return std::visit([](const auto &hdr) {
+            return hdr.e_flags;
+        }, header);
+    }
+
+    uint16_t getProgramEntrySize() {
+        return std::visit([](const auto &hdr) {
+            return hdr.e_phentsize;
+        }, header);
+    }
+
+    uint16_t getProgramEntryCount() {
+        return std::visit([](const auto &hdr) {
+            return hdr.e_phnum;
+        }, header);
+    }
+
+    uint16_t getSectionEntrySize() {
+        return std::visit([](const auto &hdr) {
+            return hdr.e_shentsize;
+        }, header);
+    }
+
+    uint16_t getSectionEntryCount() {
+        return std::visit([](const auto &hdr) {
+            return hdr.e_shnum;
+        }, header);
+    }
+
+    uint16_t getSectionStringIndex() {
+        return std::visit([](const auto &hdr) {
+            return hdr.e_shstrndx;
         }, header);
     }
 
@@ -153,6 +284,18 @@ private:
         elf_version = static_cast<ElfVersion>(getIdentData(EI_VERSION));
         elf_os_abi = static_cast<ElfOSABI>(getIdentData(EI_OSABI));
         elf_abi_version = getIdentData(EI_ABIVERSION);
+
+        // Rest of header
+        elf_machine = static_cast<ElfMachine>(getMachine());
+        elf_entry = getEntry();
+        elf_program_header_offset = getProgramOffset();
+        elf_section_header_offset = getSectionOffset();
+        elf_flags = getFlags();
+        elf_program_header_entry_size = getProgramEntrySize();
+        elf_program_header_entry_count = getProgramEntryCount();
+        elf_section_header_entry_size = getSectionEntrySize();
+        elf_section_header_entry_count = getSectionEntryCount();
+        elf_section_string_index = getSectionStringIndex();
     }
 
     [[nodiscard]] bool is64bit() const {
