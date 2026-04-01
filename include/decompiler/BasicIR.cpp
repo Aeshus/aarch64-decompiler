a3b4a5005b710bb3b2644293d746973c5c4e428f
Thomas (Aeshus) <git@aeshus.com>
2026-03-31 21:34:22 -0400

    Throw together the basic IR stuff
    It's a micro-operation thingy where I'm turning the bigger "RISC" (hehe)
    instructions into micro operations so I can then do actual analysis on
    them. Only problem is I now need to make a new glorified system on top
    of capstone... There's also a lot of quick plumbing for setting up the
    SSA stuff with temporaries and registers and flags

--- include/decompiler/BasicIR.cpp
+++ include/decompiler/BasicIR.cpp
@@ -6,49 +6,280 @@
 
 #include <stdexcept>
 #include <utility>
+#include <cstdio>
+#include <cinttypes>
 #include <capstone/capstone.h>
 #include "Elf.h"
+#include <iomanip>
 
-BasicIRInstruction::BasicIRInstruction(cs_insn instruction) {
-    operation = BasicOp::Load;
+BasicIRInstruction::BasicIRInstruction(BasicOp op, std::optional<IROperand> d,
+                                       std::optional<IROperand> s1,
+                                       std::optional<IROperand> s2)
+    : operation(op), dest(d), src1(s1), src2(s2) {
 }
 
+BasicIRInstructionBlock::BasicIRInstructionBlock(const cs_insn *instruction) {
+    address = instruction->address;
 
-BasicIRInstructionBlock::BasicIRInstructionBlock(const uint64_t address,
-                                                 std::vector<BasicIRInstruction>
-                                                 instructions) : instructions(
-        std::move(instructions)), address(
-        address) {
+    // We use detail to get a bunch of aarch64 specific stuff out
+    // If we can't get it, we've hit a bad place.
+    if (!instruction->detail)
+        throw std::runtime_error("Cannot get detail from Capstone");
+
+    cs_arm64 *arm64 = &instruction->detail->arm64;
+
+    uint32_t temp = 0;
+    auto new_vreg = [&] {
+        return IROperand::create_virt_reg(temp++);
+    };
+
+    auto operand = [&](const cs_arm64_op &op) -> IROperand {
+        switch (op.type) {
+            case ARM64_OP_REG:
+                return IROperand::create_phys_reg(op.reg);
+            case ARM64_OP_IMM:
+            case ARM64_OP_CIMM:
+                return IROperand::create_imm(op.imm);
+            case ARM64_OP_SYS:
+                return IROperand::create_imm(op.sys);
+            default: {
+                throw std::runtime_error("Failed to parse operand");
+            }
+        }
+    };
+
+    auto addr = [&](const cs_arm64_op &op) -> IROperand {
+        if (op.type == ARM64_OP_IMM) {
+            return IROperand::create_imm(op.imm);
+        }
+
+        IROperand base_addr = new_vreg();
+
+        // t# = base + offset
+        instructions.emplace_back(BasicOp::Add,
+                                  base_addr,
+                                  IROperand::create_phys_reg(op.mem.base),
+                                  IROperand::create_imm(op.mem.disp));
+
+        return base_addr;
+    };
+
+    auto writeback = [&](const cs_arm64_op &op) {
+        if (arm64->writeback && op.type == ARM64_OP_MEM) {
+            instructions.emplace_back(BasicOp::Add,
+                                      IROperand::create_phys_reg(
+                                          op.mem.base),
+                                      IROperand::create_phys_reg(
+                                          op.mem.base),
+                                      IROperand::create_imm(op.mem.disp));
+        }
+    };
+
+    switch (instruction->id) {
+        case ARM64_INS_NOP:
+            instructions.emplace_back(BasicOp::Nop);
+            break;
+
+        case ARM64_INS_ADD:
+            instructions.emplace_back(BasicOp::Add,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]),
+                                      operand(arm64->operands[2]));
+            break;
+        case ARM64_INS_SUB:
+            instructions.emplace_back(BasicOp::Sub,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]),
+                                      operand(arm64->operands[2]));
+            break;
+        case ARM64_INS_AND:
+            instructions.emplace_back(BasicOp::And,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]),
+                                      operand(arm64->operands[2]));
+            break;
+        case ARM64_INS_LSL:
+            instructions.emplace_back(BasicOp::Lsl,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]),
+                                      operand(arm64->operands[2]));
+            break;
+        case ARM64_INS_LSR:
+            instructions.emplace_back(BasicOp::Lsr,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]),
+                                      operand(arm64->operands[2]));
+            break;
+        case ARM64_INS_ASR:
+            instructions.emplace_back(BasicOp::Asr,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]),
+                                      operand(arm64->operands[2]));
+            break;
+
+        case ARM64_INS_CMP: {
+            IROperand tmp = new_vreg();
+            instructions.emplace_back(BasicOp::Sub, tmp,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]));
+            instructions.emplace_back(BasicOp::CmpEq,
+                                      IROperand::create_flag(IRFlag::Z), tmp,
+                                      IROperand::create_imm(0));
+            instructions.emplace_back(BasicOp::CmpLt,
+                                      IROperand::create_flag(IRFlag::N), tmp,
+                                      IROperand::create_imm(0));
+            break;
+        }
+
+        case ARM64_INS_MOV:
+            instructions.emplace_back(BasicOp::Mov,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]));
+            break;
+        case ARM64_INS_ADRP:
+            instructions.emplace_back(BasicOp::Adrp,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]));
+            break;
+
+        case ARM64_INS_LDR:
+        case ARM64_INS_LDRB: {
+            // TODO: need to handle the post indexing state
+            // https://stackoverflow.com/questions/66107147/instruction-writeback-in-arm
+            IROperand a = addr(arm64->operands[1]);
+            instructions.emplace_back(BasicOp::Load,
+                                      operand(arm64->operands[0]),
+                                      a);
+            writeback(arm64->operands[1]);
+            break;
+        }
+
+        case ARM64_INS_STR:
+        case ARM64_INS_STRB: {
+            IROperand a = addr(arm64->operands[1]);
+            instructions.emplace_back(BasicOp::Store, a,
+                                      operand(arm64->operands[0]));
+            writeback(arm64->operands[1]);
+            break;
+        }
+
+        case ARM64_INS_STP: {
+            IROperand base_addr = addr(arm64->operands[2]);
+            instructions.emplace_back(BasicOp::Store, base_addr,
+                                      operand(arm64->operands[0]));
+
+            IROperand offset_addr = new_vreg();
+            instructions.emplace_back(BasicOp::Add, offset_addr, base_addr,
+                                      IROperand::create_imm(8));
+            instructions.emplace_back(BasicOp::Store, offset_addr,
+                                      operand(arm64->operands[1]));
+
+            writeback(arm64->operands[2]);
+            break;
+        }
+
+        case ARM64_INS_LDP: {
+            IROperand base_addr = addr(arm64->operands[2]);
+            instructions.emplace_back(BasicOp::Load,
+                                      operand(arm64->operands[0]),
+                                      base_addr);
+
+            IROperand offset_addr = new_vreg();
+            instructions.emplace_back(BasicOp::Add, offset_addr, base_addr,
+                                      IROperand::create_imm(8));
+            instructions.emplace_back(BasicOp::Load,
+                                      operand(arm64->operands[1]),
+                                      offset_addr);
+
+            writeback(arm64->operands[2]);
+            break;
+        }
+
+        case ARM64_INS_B:
+            if (arm64->cc != ARM64_CC_INVALID && arm64->cc != ARM64_CC_AL) {
+                IROperand cond_flag = IROperand::create_flag(IRFlag::Z);
+                instructions.emplace_back(BasicOp::BranchCond,
+                                          operand(arm64->operands[0]),
+                                          cond_flag);
+            } else {
+                instructions.emplace_back(BasicOp::Branch,
+                                          operand(arm64->operands[0]));
+            }
+            break;
+
+        case ARM64_INS_BR:
+            instructions.emplace_back(BasicOp::Branch,
+                                      operand(arm64->operands[0]));
+            break;
+
+        case ARM64_INS_BL:
+            instructions.emplace_back(BasicOp::Call,
+                                      operand(arm64->operands[0]));
+            break;
+
+        case ARM64_INS_CBZ: {
+            IROperand tmp_flag = new_vreg();
+            instructions.emplace_back(BasicOp::CmpEq, tmp_flag,
+                                      operand(arm64->operands[0]),
+                                      IROperand::create_imm(0));
+            instructions.emplace_back(BasicOp::BranchCond,
+                                      operand(arm64->operands[1]), tmp_flag);
+            break;
+        }
+
+        case ARM64_INS_TBNZ: {
+            IROperand tmp_flag = new_vreg();
+            instructions.emplace_back(BasicOp::TestBit, tmp_flag,
+                                      operand(arm64->operands[0]),
+                                      operand(arm64->operands[1]));
+            instructions.emplace_back(BasicOp::BranchCond,
+                                      operand(arm64->operands[2]), tmp_flag);
+            break;
+        }
+
+        case ARM64_INS_RET:
+            instructions.emplace_back(BasicOp::Ret);
+            break;
+
+        case ARM64_INS_PACIASP:
+            instructions.emplace_back(BasicOp::Paciasp);
+            break;
+        case ARM64_INS_AUTIASP:
+            instructions.emplace_back(BasicOp::Autiasp);
+            break;
+        case ARM64_INS_BTI:
+            instructions.emplace_back(BasicOp::Bti);
+            // Safe: Removed the parameter parsing entirely
+            break;
+
+        default:
+            instructions.emplace_back(BasicOp::Unknown);
+            break;
+    }
 }
 
-BasicIR::BasicIR(const Elf &elf) : blocks({}) {
+BasicIR::BasicIR(const Elf &elf) : instruction_blocks({}) {
     auto sections = elf.get_executable_sections();
-
     csh handle;
 
     if (cs_open(CS_ARCH_ARM64, CS_MODE_LITTLE_ENDIAN, &handle) != CS_ERR_OK) {
         throw std::runtime_error("Couldn't open capstone");
     }
 
-    for (auto section: sections) {
-        auto start = reinterpret_cast<uint8_t *>(elf.data + section->sh_offset);
+    cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
+
+    for (const auto section: sections) {
+        const auto start = reinterpret_cast<uint8_t *>(
+            elf.data + section->sh_offset);
         cs_insn *insn;
 
-        size_t count = cs_disasm(handle, start, section->sh_size,
-                                 section->sh_addr, 0,
-                                 &insn);
+        const size_t count = cs_disasm(handle, start, section->sh_size,
+                                       section->sh_addr, 0, &insn);
 
-        std::vector<cs_insn> ins{insn, insn + count};
-
-        std::vector<BasicIRInstruction> v{};
-        for (auto i: ins) {
-            BasicIRInstruction is{i};
-            v.push_back(is);
+        instruction_blocks.reserve(instruction_blocks.size() + count);
+        for (size_t j = 0; j < count; ++j) {
+            instruction_blocks.emplace_back(&insn[j]);
         }
-
-        BasicIRInstructionBlock instructions{section->sh_addr, v};
-
-        blocks.push_back(instructions);
         cs_free(insn, count);
     }
 
--- include/decompiler/BasicIR.h
+++ include/decompiler/BasicIR.h
@@ -4,7 +4,9 @@
 
 #ifndef DECOMPILER_BASICIR_H
 #define DECOMPILER_BASICIR_H
+
 #include <string>
+#include <variant>
 #include <vector>
 #include <optional>
 #include <capstone/capstone.h>
@@ -12,37 +14,95 @@
 #include "Elf.h"
 
 enum class BasicOp {
+    Nop,
     Add,
+    Sub,
+    And,
+    Lsl,
+    Lsr,
+    Asr,
+    Mov,
+    Branch,
+    BranchCond,
+    Call,
+    Ret,
     Load,
+    Store,
+    SetFlag,
+    Adrp,
+    Paciasp,
+    Autiasp,
+    Bti,
+    CmpEq,
+    CmpNe,
+    CmpLt,
+    CmpGt,
+    TestBit,
+    Unknown
 };
 
-// x0 = IADD(x1, x2)
-// t = Dereference(x1)
-// x1 = LOAD(t)
+enum class IROpType {
+    PhysicalReg,
+    VirtualReg,
+    Immediate,
+    Label,
+    Flag
+};
+
+enum class IRFlag { Z, N, C, V };
+
+struct IROperand {
+    IROpType type;
+    std::variant<arm64_reg, uint32_t, uint64_t, IRFlag> value;
+
+    uint32_t ssa_version = 0;
+
+    static IROperand create_phys_reg(arm64_reg reg) {
+        return {IROpType::PhysicalReg, reg, 0};
+    }
+
+    static IROperand create_virt_reg(uint32_t v_reg_id) {
+        return {IROpType::VirtualReg, v_reg_id, 0};
+    }
+
+    static IROperand create_imm(uint64_t imm) {
+        return {IROpType::Immediate, imm, 0};
+    }
+
+    static IROperand create_label(uint64_t addr) {
+        return {IROpType::Label, addr, 0};
+    }
+
+    static IROperand create_flag(IRFlag flag) {
+        return {IROpType::Flag, flag, 0};
+    }
+};
+
 struct BasicIRInstruction {
     BasicOp operation;
-    std::optional<std::string> first;
-    std::optional<std::string> second;
-    std::optional<std::string> operand;
+    std::optional<IROperand> dest;
+    std::optional<IROperand> src1;
+    std::optional<IROperand> src2;
 
-    explicit BasicIRInstruction(cs_insn instruction);
+    BasicIRInstruction(BasicOp op,
+                       std::optional<IROperand> d = std::nullopt,
+                       std::optional<IROperand> s1 = std::nullopt,
+                       std::optional<IROperand> s2 = std::nullopt);
 };
 
 struct BasicIRInstructionBlock {
-    std::vector<BasicIRInstruction> instructions;
     uint64_t address;
+    std::vector<BasicIRInstruction> instructions;
 
-    explicit BasicIRInstructionBlock(uint64_t address,
-                                     std::vector<BasicIRInstruction>
-                                     instructions);
+    explicit BasicIRInstructionBlock(const cs_insn *instruction);
 };
-
 
 struct BasicIR {
-    std::vector<BasicIRInstructionBlock> blocks;
+    std::vector<BasicIRInstructionBlock> instruction_blocks;
 
     explicit BasicIR(const Elf &elf);
-};
 
+    void print() const;
+};
 
 #endif //DECOMPILER_BASICIR_H
