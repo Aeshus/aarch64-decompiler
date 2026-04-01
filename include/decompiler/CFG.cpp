48864bbf918a80f05b695a5d288216b6aa271e06
Thomas (Aeshus) <git@aeshus.com>
2026-03-31 22:12:45 -0400

    Scaffold CFG

---
+++ include/decompiler/CFG.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aeshus on 3/31/26.
+//
+
+#include "CFG.h"
---
+++ include/decompiler/CFG.h
@@ -0,0 +1,13 @@
+//
+// Created by aeshus on 3/31/26.
+//
+
+#ifndef DECOMPILER_CFG_H
+#define DECOMPILER_CFG_H
+
+
+class CFG {
+};
+
+
+#endif //DECOMPILER_CFG_H
